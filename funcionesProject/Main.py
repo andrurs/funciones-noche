d66d2d9c6168c861bd97b6ca95efa722c995ed50
Andres Rodriguez <andresr.sistemas@gmail.com>
2024-10-03 18:52:05 -0500

    crear proptearchivo main para probar funcion

---
+++ funcionesProject/Main.py
@@ -0,0 +1,10 @@
+#definir una función
+def my_function():
+    print("Hello from a function")
+
+  #definir una funcion con argumento
+def mostrarName(nombre):
+    print("Su nombre es: "+nombre)
+nom= input("Ingrese su nombre: ")
+  #Invocar la funcion
+mostrarName(nom)
